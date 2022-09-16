f4ebb951c32b01594cf71d1a55c74f7a4f5db9bc
NGTO-WONG <44777832+NGTO-WONG@users.noreply.github.com>
2022-09-16 22:10:36 +0800

    Update NewBehaviourScript.cs
    测试冲突

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+//别人写的代码
     }
 
     // Update is called once per frame
