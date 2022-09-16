c5d844c18e0169fe17c16b7c178b1ee1ec13bbed
LatifaFleuranza <505774621@qq.com>
2022-09-16 23:02:48 +0800

    初始化场景。

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -7,53 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("A");
-      Debug.Log("A");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-                Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("A");        
-Debug.Log("A");
-                Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("B");        
-Debug.Log("A");
-  Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("A");        
-Debug.Log("A");
-                Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("B");        
-Debug.Log("A");
-Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("A");        
-Debug.Log("A");
-                Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("B");        
-Debug.Log("A");
-  Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("A");        
-Debug.Log("A");
-                Debug.Log("A");        
-Debug.Log("A");      
-  Debug.Log("A");        
-Debug.Log("B");        
-Debug.Log("A");
-Debug.Log("A");
+        
     }
 }
