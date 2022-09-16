961e39a232e71a4f062ea1b8b98c0c9d7ee238cf
LatifaFleuranza <505774621@qq.com>
2022-09-16 22:30:40 +0800

    1

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("A");
+        Debug.Log("ASD");
     }
 
     // Update is called once per frame
